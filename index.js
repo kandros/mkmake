790580192f3514acd15f199d2df3f88282b641f1
Jaga Santagostino <jagasantagostino@gmail.com>
2019-11-08 13:47:11 +0100

    initla commit

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const { Snippet } = require('enquirer')
+const fs = require('fs')
+
+const prompt = new Snippet({
+  name: 'makefile',
+  message: 'Fill out commands for Makefile',
+  required: true,
+  fields: [
+    {
+      name: 'runCommand',
+      message: 'run Command',
+    },
+    {
+      name: 'devCommand',
+      message: 'dev Command',
+    },
+  ],
+  template: `run: 
+\t\${runCommand}
+dev:
+\t\${devCommand}
+`,
+})
+
+prompt
+  .run()
+  .then(toFile('Makefile'))
+  .catch(console.error)
+
+function toFile(filename) {
+  return function(answer) {
+    console.log(`creted ${filename}`)
+    fs.writeFileSync(filename, answer.result, { encoding: 'utf-8' })
+  }
+}
